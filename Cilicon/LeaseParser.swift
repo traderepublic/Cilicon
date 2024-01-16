531761d4cc641b4aa17ce35dbb17ad00289494d4
Marcocanc <marco.cancellieri@traderepublic.com>
2024-01-16 16:40:35 +0100

    Remove unused code

--- Cilicon/LeaseParser.swift
+++ Cilicon/LeaseParser.swift
@@ -59,23 +59,3 @@ enum LeaseParser {
         }
     }
 }
-
-struct MACAddress: Equatable, Hashable, CustomStringConvertible {
-    var mac: [UInt8] = Array(repeating: 0, count: 6)
-
-    init?(fromString: String) {
-        let components = fromString.components(separatedBy: ":")
-
-        if components.count != 6 {
-            return nil
-        }
-
-        for (index, component) in components.enumerated() {
-            mac[index] = UInt8(component, radix: 16)!
-        }
-    }
-
-    var description: String {
-        String(format: "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5])
-    }
-}
