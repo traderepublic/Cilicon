4711392cd06d2f2cd55168ae9bf78556f12df320
mrbart <37536053+FabianBartels@users.noreply.github.com>
2024-09-10 18:09:12 +0200

    Pass config as launch argument to support different config file location (#58)
    * Pass config as launch argument to support different config file location
    
    * ~adjusted error naming
    
    * Update ConfigManager.swift
    
    ---------
    
    Co-authored-by: Bartels Fabian (LDC) <Fabian.Bartels@liebherr.com>

--- Cilicon/Config/ConfigManager.swift
+++ Cilicon/Config/ConfigManager.swift
@@ -2,18 +2,24 @@ import Foundation
 import Yams
 
 class ConfigManager {
-    static let configPaths = ["/cilicon.yml", "/.cilicon.yml"]
-        .map { NSHomeDirectory() + $0 }
-
-    static var fileExists: Bool {
-        configPaths.map(FileManager.default.fileExists).contains(true)
-    }
 
     let config: Config
 
+    /// Default config
+    static let fallbackConfigPath = ["/cilicon.yml", "/.cilicon.yml"]
+        .map { NSHomeDirectory() + $0 }
+        .filter(FileManager.default.fileExists).first
+
     init() throws {
         let decoder = YAMLDecoder()
-        guard let data = Self.configPaths.compactMap(FileManager.default.contents).first else {
+
+        // Launch argument to config e.g. ~/cilicon.yml
+        // If no launch argument is found. The default fallback config is used
+        guard let configPath = UserDefaults.standard.string(forKey: "config-path") ?? Self.fallbackConfigPath,
+              FileManager.default.fileExists(atPath: configPath) else {
+            throw ConfigManagerError.fileDoesNotExist
+        }
+        guard let data = FileManager.default.contents(atPath: configPath) else {
             throw ConfigManagerError.fileCouldNotBeRead
         }
         self.config = try decoder.decode(Config.self, from: data)
@@ -22,4 +28,5 @@ class ConfigManager {
 
 enum ConfigManagerError: Error {
     case fileCouldNotBeRead
+    case fileDoesNotExist
 }
